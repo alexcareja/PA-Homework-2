7b2ec24b489699401a0bafc2504e691df06d44dc
Alex Careja <alexandrucareja@yahoo.com>
2020-05-17 05:03:13 -0400

    Add files via upload
    P1: - done
    queue -> list
    + list
    + comments
    - queue

--- holograme.c
+++ holograme.c
@@ -1,144 +1,148 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct QueueNode {
+typedef struct ListNode {
 	int elem;
-	struct QueueNode *next;
-}QueueNode;  // tipul de data nod dintr-o coada
+	struct ListNode *next;
+}ListNode;  // tip de date nod de lista
 
-typedef struct Queue {
-	QueueNode *front;
-	QueueNode *rear;
-	int size;
-}Queue;  // tipul de data coada
+typedef struct List {
+	ListNode *start;
+	ListNode *end;
+}List;  // tip de date lista
 
-Queue* createQueue(void) {  // crearea unei cozi vide
-	Queue *q = (Queue *) malloc(sizeof(Queue));
-	q->front = NULL;
-	q->rear = NULL;
-	q->size = 0;
-	return q;
+List *initList() {  // initializeaza o lista goala
+	List *l = (List *) malloc(sizeof(List));
+	l->start = NULL;
+	l->end = NULL;
+	return l;
 }
 
-int isQueueEmpty(Queue *q) {  // verificare coada goala
-	if (q->front == NULL) {
-		return 1;
-	}
-	return 0;
-}
-
-void enqueue(Queue *q, int elem) {  // inscriere in coada
-	q->size += 1;
-	QueueNode *node = (QueueNode *) malloc(sizeof(QueueNode));
+void insertInList(List *l, int elem) {  // insereaza un nod in lista
+	ListNode *node = (ListNode *) malloc(sizeof(ListNode));
 	node->elem = elem;
 	node->next = NULL;
-	if (isQueueEmpty(q)) {
-		q->front = node;
-		q->rear = node;
+	if (l->start == NULL) {  // lista e goala
+		// insereaza la inceputul listei
+		l->start = node;
+		l->end = node;
 		return;
 	}
-	q->rear->next = node;
-	q->rear = node;
+	// insereaza la finalul listei
+	l->end->next = node;
+	l->end = node;
 }
 
-int front(Queue* q) {  // vezi primul element din coada
-	return q->front->elem;
-}
-
-void dequeue(Queue* q) {  // scoate primul element din coada
-	if (isQueueEmpty(q)) {
+void deleteFirst(List *l) {  // sterge primul nod din lista
+	if (l->start == NULL) {
 		return;
 	}
-	q->size -= 1;
-	QueueNode *aux = q->front;
-	q->front = q->front->next;
-	if (q->front == NULL) {
-		q->rear = NULL;
+	ListNode *aux = l->start;
+	l->start = aux->next;
+	if (l->start == NULL) {  // daca am eliminat ultimul element din lista
+		l->end = NULL;
 	}
 	free(aux);
 }
 
-void destroyQueue(Queue *q) {  // distruge coada
-	while(!isQueueEmpty(q)) {
-		dequeue(q);
+void destroyList(List *l) {  // distruge lista, elibereaza memoria
+	while (l->start != NULL) {
+		deleteFirst(l);
 	}
-	free(q);
+	free(l);
 }
 
-void DFS_conex(Queue **va, Queue *q, int *v, int current);
-int aciclic(Queue *q, Queue **va, int *v);
-int DFS_ciclu(Queue **va, int *v, int current);
+void DFS_conex(List **la, List *q, int *v, int current);
+int aciclic(List *q, List **la, int *v);
+int DFS_ciclu(List **la, int *v, int current);
 
 int main() {
 	int ans, i, n, m, u , v;
-	FILE *fin = fopen("holograme.in", "r");
+
+	// citeste N si M
+	FILE *fin = fopen("holograme.in", "r");  // fisier input
 	fscanf(fin, "%d %d\n", &n, &m);
-	ans = n;
-	int *viz = (int *) calloc(n + 1, sizeof(int));
-	Queue **va = (Queue **) malloc((n + 1) * sizeof(Queue *));
-	// ^ vector de adiacenta pentru graful orientat
-	Queue **va_no = (Queue **) malloc((n + 1) * sizeof(Queue *));
-	// ^ vector de adiacenta pentru graful neorientat
+
+	// initializari
+	ans = n;  // variabila raspuns initilizata cu N (nr maxim de holograme)
+	int *viz = (int *) calloc(n + 1, sizeof(int));  // vector de vizite
+	List **la = (List **) malloc((n + 1) * sizeof(List *));
+	// ^ lista de adiacenta pentru graful orientat
+	List **la_no = (List **) malloc((n + 1) * sizeof(List *));
+	// ^ lista de adiacenta pentru graful neorientat
 	for (i = 1; i <= n; i++) {
-		va[i] = createQueue();
-		va_no[i] = createQueue();
+		la[i] = initList();
+		la_no[i] = initList();
 	}
+
+	// citire perechi
 	for (i = 0; i < m; i++) {
 		fscanf(fin, "%d %d\n", &u, &v);
-		enqueue(va[u], v);
-		enqueue(va_no[u], v);
-		enqueue(va_no[v], u);
+
+		// construieste lista de adiacente pentru graful orientat
+		insertInList(la[u], v);
+
+		// construieste lista de adiacente pentru graful neorientat
+		insertInList(la_no[u], v);
+		insertInList(la_no[v], u);
 	}
 	fclose(fin);
-	// gaseste componentele conexe si introduce nodurile sale in coada q
+
+	// gaseste componentele conexe si introduce nodurile sale in lista q
 	// considerand graful ca fiind neorientat
 	for (i = 1; i <= n; i++) {
 		if (viz[i] == 0) {
-			Queue *q = createQueue();
-			DFS_conex(va_no, q, viz, i);
+			List *q = initList();
+			DFS_conex(la_no, q, viz, i);
 			// pentru componenta conexa gasita,verifica daca este aciclica
 			// considerand graful orientat de data aceasta
-			if (aciclic(q, va, viz)) {
+			if (aciclic(q, la, viz)) {
 				// daca nu are ciclu, atunci scad 1 din numarul minim de muchii
 				ans--;
 			}
+			destroyList(q);
 		}
 	}
-	FILE *fout = fopen("holograme.out", "w");
+
+	// scrie rezultat
+	FILE *fout = fopen("holograme.out", "w");  // fisier output
 	fprintf(fout, "%d\n", ans);
+	fclose(fout);
+
+	// elibereaza memoria
 	for (i = 1; i <= n; i++) {
-		destroyQueue(va[i]);
-		destroyQueue(va_no[i]);
+		destroyList(la[i]);
+		destroyList(la_no[i]);
 	}
-	free(va);
-	free(va_no);
+	free(la);
+	free(la_no);
 	free(viz);
 	return 0;
 }
 
-void DFS_conex(Queue **va, Queue *q, int *v, int current) {
+void DFS_conex(List **la, List *q, int *v, int current) {
 	// construieste in q o componenta conexa prin DFS pe graful neorientat
 	int succesor;
 	v[current] = 1;
-	enqueue(q, current);  // adauga in componenta conexa q, nodul curent
-	QueueNode *aux = va[current]->front;
+	insertInList(q, current);  // adauga in componenta conexa q, nodul curent
+	ListNode *aux = la[current]->start;
 	while (aux != NULL) {
 		succesor = aux->elem;
 		if (v[succesor] == 0) {
 			// daca am gasit un nod nevizitat, viziteaza
-			DFS_conex(va, q, v, succesor);
+			DFS_conex(la, q, v, succesor);
 		}
 		aux = aux->next;
 	}
 }
 
-int aciclic(Queue *q, Queue **va, int *v) {
+int aciclic(List *q, List **la, int *v) {
 	// verifica daca o componenta conexa contine un ciclu
-	QueueNode *aux = q->front;
+	ListNode *aux = q->start;
 	while (aux != NULL) {
 		if (v[aux->elem] == 1) {
 			// daca nodul nu a fost vizitat, aplica DFS pentru a gasi ciclu
-			if (DFS_ciclu(va, v, aux->elem)) {
+			if (DFS_ciclu(la, v, aux->elem)) {
 				// daca am gasit un ciclu, returneaza 0
 				return 0;
 			}
@@ -149,17 +153,17 @@ int aciclic(Queue *q, Queue **va, int *v) {
 	return 1;
 }
 
-int DFS_ciclu(Queue **va, int *v, int current) {
+int DFS_ciclu(List **la, int *v, int current) {
 	// cauta daca exista un ciclu
 	int succesor;
 	v[current] = 2;  // nod gri
-	QueueNode *aux = va[current]->front;
+	ListNode *aux = la[current]->start;
 	while (aux != NULL) {
 		succesor = aux->elem;
 		if (v[succesor] == 2) {  // daca am gasit un nod gri, atunci avem ciclu
 			return 1;
 		}
-		if (DFS_ciclu(va, v, succesor)) {
+		if (DFS_ciclu(la, v, succesor)) {
 			// daca functia recursiva a descoperit un ciclu, returneza 1
 			return 1;
 		}
